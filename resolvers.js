2f5304c7f82980c789474784e554e2b48e96e96e
Anton Semenkov <k3gbdia1@gmail.com>
2022-05-25 18:33:22 +0300

    import data

--- resolvers.js
+++ resolvers.js
@@ -1,12 +1,4 @@
-const authors =[
-    { id: 5, name: 'Lev', booksId:[1,3] },
-    { id: 6, name: 'Gorky', booksId:[2] }
-]
-const books = [
-    { id: 1, title: 'First' },
-    { id: 2, title: 'Second' },
-    { id: 3, title: 'Third' }
-];
+import { authors, books } from './simpleData.js'
 
 export const resolvers = {
     Query: {
